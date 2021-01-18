ae9fae4816130692a7d33a8b1f5d5999399daaa9
bbrintle <bbrintle@gmail.com>
2021-01-18 13:04:07 -0800

    heroku deploy

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -6,7 +6,7 @@ function Jumbotron() {
   return (
     <div className="jumbotron text-center">
       <h1><span><a className="white" id="jumboHead" href="/">Blake's Google Book Search</a></span></h1>
-        <div>
+        <div className='pwrdBy'>
           <a className="white" target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
             Powered by Google Books
           </a>
